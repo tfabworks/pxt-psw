6c5115fb818d8699bfb22e85bebf58938d386a1d
atsushi morita <himarayayukiotoko@gmail.com>
2022-03-14 18:31:39 +0900

    update

--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
 basic.forever(function () {
-    if (kkk.is_man_moving()) {
-        kkk.turn_on()
+    if (PSW.is_man_moving()) {
+        PSW.turn_on()
     } else {
-        kkk.turn_off()
+        PSW.turn_off()
     }
 })
 basic.forever(function () {
-    basic.showNumber(kkk.light_level())
+    basic.showNumber(PSW.light_level())
 })
